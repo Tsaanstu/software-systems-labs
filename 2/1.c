4d4227915451422c63ce4dbff228e1bedc992e9e
Tsaanstu <atasunts.vladimir@yandex.ru>
2019-03-25 17:14:26 +0300

    _

--- 2/1.c
+++ 2/1.c
@@ -1,42 +1,315 @@
-#include <stdio.h>
-#include <pthread.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <signal.h>
+#include <ctype.h>
 
-//потоковая функция
+#define _REENTRANT
 
+#include <pthread.h>
+#include <sched.h>
+#define BUF_SIZE 256
+extern int errno;
 
+pthread_t itid, ctid;
+pthread_mutex_t mutx1, mutx2;
+pthread_cond_t condx1, condx2;
+int msgline1 = 0, msgline2 = 0;
+int flag11 = 1, flag12 = 0, flag13 = 0, flag14 = 0, flag21 = 1, flag22 = 0, flag23 = 0, flag24 = 0;
+char msgbuf1[BUF_SIZE], msgbuf2[BUF_SIZE];
 
-void* first_thread(void* thread_data){
-	sleep(4);
-	printf("1\n");
-	pthread_exit(0);
+void clean(char *line) {
+    int j;
+    for (j = 0; j < BUF_SIZE; j++)
+        line[j] = '\0';
 }
 
-void* second_thread(void* thread_data){
-	sleep(2);
-	printf("2\n");
-	pthread_exit(0);
+void func(int s) {
+    char c[3];
+    write(1, "\nSigcautched\n", 14);
+    read(0, c, 3);
+    if (!strncmp(c, "1.1", 3)) {
+        flag12 = flag13 = flag14 = 0;
+        flag11 = 1;
+    } else if (!strncmp(c, "1.2", 3)) {
+        flag11 = flag13 = flag14 = 0;
+        flag12 = 1;
+    } else if (!strncmp(c, "1.3", 3)) {
+        flag11 = flag12 = flag14 = 0;
+        flag13 = 1;
+    } else if (!strncmp(c, "1.4", 3)) {
+        flag11 = flag12 = flag13 = 0;
+        flag14 = 1;
+    } else if (!strncmp(c, "2.1", 3)) {
+        flag22 = flag23 = flag24 = 0;
+        flag21 = 1;
+    } else if (!strncmp(c, "2.2", 3)) {
+        flag21 = flag23 = flag24 = 0;
+        flag22 = 1;
+    } else if (!strncmp(c, "2.3", 3)) {
+        flag22 = fl
+        ag21 = flag24 = 0;
+        flag23 = 1;
+    } else if (!strncmp(c, "2.4", 3)) {
+        flag22 = flag23 = flag21 = 0;
+        flag24 = 1;
+    } else {
+        pthread_cond_signal(&condx1);
+    }
 }
 
-int main(int argc, char **argv) {
+int trans(int sig) {
+    char c, buf[BUF_SIZE];
+    int i, j, l;
+    pthread_mutex_lock(&mutx1);
+    if (!flag11) {
+        pthread_mutex_unlock(&mutx1);
+        return 1;
+    }
+    write(1, "I1\n", 3);
+    strncpy(msgbuf2, msgbuf1, msgline1);
+    clean(msgbuf1);
+    write(1, msgbuf2, msgline1);
+    msgline2 = msgline1;
+    msgline1 = 0;
+    pthread_mutex_unlock(&mutx1);
+    pthread_cond_sig
+            nal(&condx1);
+    pthread_cond_wait(&condx2, &mutx2);
+    usleep(10);
+}
 
-	//какие то данные для потока (для примера)
-	void* thread_data = NULL;
+int invert(int sig) {
+    char c;
+    int i, l, j;
+    pthread_mutex_lock(&mutx1);
+    if (!flag12) {
+        pthread_mutex_unlock(&mutx1);
+        return 1;
+    }
+    write(1, "I2\n", 3);
+    strncpy(msgbuf2, msgbuf1, ms
+    gline1);
+    clean(msgbuf1);
+    msgline2 = msgline1;
+    msgline1 = 0;
+    pthread_mutex_unlock(&mutx1);
+    pthread_cond_signal(&condx1);
+    l = msgline2;
+    for (i = 0; i < (l / 2); i++) {
+        c = msgbuf2[i];
+        msgbuf2[i] = msgbuf2[l-2-i];
+        msgbuf2[l-2-i] = c;
+    }
+    write(1, msgbuf2, msgline2);
+    pthread_cond_wait(&condx2, &mutx2);
+    usleep(10);
+}
 
-	//создаем идентификатор потока
-	pthread_t f_thread, s_thread;
+int change(int sig) {
+    char c;
+    int i, j, l;
+    pthread_mutex_lock(&mutx1);
+    if (!flag13) {
+        pthread_mutex_unlock(&mutx1);
+        return 1;
+    }
+    write(1, "I3\n", 3);
+    strncpy(m
+    sgbuf2, msgbuf1, msgline1);
+    clean(msgbuf1);
+    msgline2 = msgline1;
+    msgline1 = 0;
+    pthread_mutex_unlock(&mutx1);
+    pthread_cond_signal(&condx1);
+    l = msgline2;
+    for (i = 0; i < l-2; i += 2) {
+        c = msgbuf2[i];
+        msgbuf2[i] = msgbuf2[i + 1];
+        msgbuf2[i + 1] = c;
+    };
+    write(1, msgbuf2, msgline2);
+    pthread_cond_wait(&condx2, &mutx2);
+    usleep(10);
+}
 
-	//создаем поток по идентификатору thread и функции потока threadFunc
-	//и передаем потоку указатель на данные thread_data
-	pthread_create(&f_thread, NULL, first_thread, thread_data);
-	pthread_create(&s_thread, NULL, second_thread, thread_data);
+int koi(int sig) {
+    char c;
+    int i, l, j;
+    pthread_mutex_lock(&mutx1);
+    if (!flag14) {
+        pthread_mutex_unlock(&mutx1);
+        return 1;
+    }
+    write(1, "I4\n", 3);
+    strncpy(msgbuf2, msgbuf1, msgline1);
+    clean(msgbuf1);
+    msgline2 = msgline1;
+    msgline1 = 0;
+    pthread_mutex_unlock(&mutx1);
+    pthread_cond_signal(&condx1);
+    l = msgline2;
+    for (i = 0; i < l - 1; i++) {
+        if (isupper(msgbuf2[i])) msgbuf2[i] += 32;
+        else if (islower(msgbuf2[i]))
+            msgbuf2[i] -= 32;
+        msgbuf2[i] |= 1 << 7;
+    };
+    write(1, msgbuf2, msgline2);
+    pthread_cond_wait(&condx2, &mutx2);
+    usleep(10);
+}
 
-	printf("main\n");
+void *invertor(void *arg_p) {
+    char c;
+    write(1, "I start\n", 8);
+    pthread_mutex_lock(&mutx2);
+    while (1) {
+        if (flag11) {
+            trans(1);
+        }
+        if (flag12) {
+            invert(1);
+        }
+        if (flag13) {
+            change(1);
+        }
+        if (flag14) {
+            koi(1);
+        }
+    }
+}
 
-	//ждем завершения потока
+int trans1(int sig) {
+    int i, j, l;
+    pthread_mutex_lock(&mutx2);
+    if (!flag21) {
+        pthread_mutex_unlock(&mutx
+        2);
+        return 1;
+    }
+    write(1, "C1\n", 3);
+    write(1, msgbuf2[i], msgline2);
+    clean(msgbuf2);
+    msgline2 = 0;
+    pthread_mutex_unlock(&mutx2);
+    pthread_cond_signal(&condx2);
+    usleep(10);
+}
 
-	pthread_join(f_thread, NULL);
-	pthread_join(s_thread, NULL);
+int up(int sig) {
+    char c, buf[BUF_SIZE];
+    int i, j, len;
+    pthread_m
+            utex_lock(&mutx2);
+    if (!flag22) {
+        pthread_mutex_unlock(&mutx2);
+        return 1;
+    }
+    write(1, "C2\n", 3);
+    strncpy(buf, msgbuf2, msgline2);
+    clean(msgbuf2);
+    len = msgline2;
+    msgline2 = 0;
+    pthread_mutex_unlock(&mutx2);
+    pthread_cond_signal(&condx2);
+    usleep(10);
+    for (i = 0; i < len; i++) {
+        buf[i] = toupper(buf[i]);
+    }
+    write(1, buf, len);
+    clean(buf);
+}
 
-	return 0;
+int down(int sig) {
+    char c, buf[BUF_SIZE];
+    int i, j, len, l;
+    pthread_mutex_lock(&mutx2);
+    if (!flag23) {
+        pthread_mutex_unlock(&mutx2);
+        return 1;
+    }
+    write(1, "C3\n", 3);
+    strncpy(buf, msgbuf2, msgline2);
+    clean(msgbuf2[i]);
+    len = msgline2;
+    msgline2 = 0;
+    pthread_mutex_unlock(&mutx2);
+    pthread_cond_signal(&condx2);
+    usleep(10);
+    for (i = 0; i < len; i++) {
+        buf[i] = tolower(buf[i]);
+    }
+    write(1, buf, len);
+    clean(buf);
+}
+
+int convert(int sig) {
+    char c, buf[BUF_SIZE];
+    int i, l, j, len;
+    pthread_mutex_lock(&mutx2);
+    if (!flag24) {
+        pthread_mutex_unlock(&mutx2);
+        return 1;
+    }
+    write(1, "C4\n", 3);
+    strncpy(buf, msgbuf2, msgline2);
+    clean(msgbuf2);
+    len = msgline2;
+    msgline2 = 0;
+    pthread_mutex_unlock(&mutx2);
+    pthread_cond_signal(&condx2);
+    usleep(10);
+    for (i = 0; i < len; i++) {
+        if (isupper(buf[i]))
+            buf[i] = tolower(buf[i]);
+        else
+            buf[i] = toupper(buf[i]);
+    };
+    write(1, buf, len);
+    clean(buf);
+}
+
+void *consumer(void *arg_p) {
+    write(1, "C start\n", 8);
+    while (1) {
+        if (flag21) {
+            trans1(1);
+        }
+        if (flag22) {
+            up(1);
+        }
+        if (flag23) {
+            down(1);
+        }
+        if (flag24) {
+            convert(1);
+        }
+    };
+}
+
+int main() {
+    pthread_attr_t pattr;
+    int ret;
+    write(1, "P start\n", 8);
+    pthread_attr_init(&pattr);
+    pthread_attr_setscope(&pattr, PTHREAD_SCOPE_SYSTEM);
+    pthread_attr_setdetachstate(&pattr, PTHREAD_CREATE_JOINABLE);
+    pthread_mutex_init(&mutx1, NULL);
+    pthread_cond_init(&condx1, NULL);
+    pthread_mutex_init(&mutx2, NULL);
+    pthread_cond_init(&condx2, NULL);
+    if (ret = pthread_create(&itid, &pattr, invertor, NULL))
+        perror("pthread_create");
+    if (ret = pthread_create(&ctid, &pattr, consumer, NULL))
+        perror("pthread_create");
+    signal(SIGINT, func);
+    pthread_mutex_lock(&mutx1);
+    while (1) {
+        msgline1 = read(0, msgbuf1, BUF_SIZE);
+        if (msgbuf1[0] == 27) raise(SIGQUIT);
+        if (msgbuf1[0] != '\n')
+        pthread_cond_wait(&condx1, &mutx1);
+    }
 }
--- 2/2.c
+++ 2/2.c
@@ -7,9 +7,92 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <signal.h>
+#include <string.h>
 
-void 
+#define BUF_SIZE 256
+
+unsigned short int modes[2];
+pthread_t itid, ctid;
+pthread_mutex_t mutx1, mutx2;
+pthread_cond_t condx1, condx2;
+char str_in_first_thread[BUF_SIZE];
+
+void read_commands();
+
+_Bool init_work_mode(int *mode);
+
+void *first_thread(void *arg_p);
+
+void *second_thread(void *arg_p);
+
 
 int main(int argc, char **argv) {
+    pthread_attr_t pattr;
+    int ret;
+    write(1, "P start\n", 8);
+    pthread_attr_init(&pattr);
+    pthread_attr_setscope(&pattr, PTHREAD_SCOPE_SYSTEM);
+    pthread_attr_setdetachstate(&pattr, PTHREAD_CREATE_JOINABLE);
+    pthread_mutex_init(&mutx1, NULL);
+    pthread_cond_init(&condx1, NULL);
+    pthread_mutex_init(&mutx2, NULL);
+    pthread_cond_init(&condx2, NULL);
+    if (ret = pthread_create(&itid, &pattr, first_thread, NULL)) {
+        perror("pthread_create");
+    }
+    if (ret = pthread_create(&ctid, &pattr, second_thread, NULL)) {
+        perror("pthread_create");
+    }
 
+    signal(SIGINT, read_commands);
+    while (1) {
+        read(0, str_in_first_thread, BUF_SIZE);
+        if (str_in_first_thread[0] == '~') {
+            exit(0);
+        }
+        if (str_in_first_thread[0] != '\n') {
+            pthread_cond_wait(&condx1, &mutx1);
+        }
+    }
+}
+
+void *first_thread(void *arg_p) {
+    while (1) {
+        switch (modes[0]) {
+            default:
+                printf("1 thread kek\n");
+            break;
+        }
+    }
+}
+
+void *second_thread(void *arg_p) {
+    while (1) {
+        switch (modes[1]) {
+            default:
+                printf("2 thread lol\n");
+            break;
+        }
+    }
+}
+
+void read_commands() {
+    char buf[3];
+    char *answer[] = {"\nsignal \"ctrl+c\" is processed\n",
+                      "input first and second thread mode (two 1-4 int numbers)\nExample: 14\n",
+                      "input error\n"
+    };
+    write(1, answer[0], strlen(answer[0]));
+    unsigned short int mode = 0, i;
+    write(1, answer[1], strlen(answer[1]));
+    read(0, buf, 3);
+    for (i = 0; i < 2; i++) {
+        mode = buf[i] - '0';
+        if (mode < 1 || mode > 4) {
+            write(1, answer[2], strlen(answer[2]));
+            exit(1);
+        }
+        modes[i] = mode;
+    }
+    printf("%d %d\n", modes[0], modes[1]);
 }
