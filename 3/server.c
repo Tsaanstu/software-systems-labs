accfb6ff6b4da6a16434051457f990d5df430b69
Tsaanstu <atasunts.vladimir@yandex.ru>
2019-03-28 14:49:58 +0300

    example

--- 3/server.c
+++ 3/server.c
@@ -2,101 +2,72 @@
 // Created by tsaanstu on 27.03.19.
 //
 
+/* определяет типы данных */
 #include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
+/* "Главный" по сокетам */
 #include <sys/socket.h>
+/* sockaddr_in struct, sin_family, sin_port, in_addr_t, in_port_t, ...*/
 #include <netinet/in.h>
-#include <netdb.h>
-#include <errno.h>
-#include <memory.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+
 #include <stdio.h>
-#include <stdlib.h>
-#include <signal.h>
+#include <memory.h>
+#include <string.h>
+#include <errno.h>
 
-
-
-#define SRV_PORT 1234
-#define RM "rm "
-#define TAR_C 128 // длина команды tar
-
-struct stat st = {0};
-int from_len;
-int fd = 0;
-char buf[BUF_SIZE];
-char path[PATH_SIZE];
-char tar[TAR_C];
-char rm_tar[TAR_C];
-char tar_path[TAR_C];
-int path_l, name_l;
-
-static void _mkdir(const char *dir) { // функция рекурсивно создающая директорию на сервере
-  char tmp[256];
-  char *p = NULL;
-  size_t len;
-  snprintf(tmp, sizeof(tmp), "%s", dir);
-  len = strlen(tmp);
-  if (tmp[len - 1] == '/') tmp[len - 1] = 0;
-  for (p = tmp + 1; *p; p++)
-    if (*p == '/') {
-      *p = 0;
-      mkdir(tmp, S_IRWXU);
-      *p = '/';
-    }
-  mkdir(tmp, S_IRWXU);
-}
-void siginthandler() {
-  if (fd) close(fd);
-  shutdown(s_new, 0);
-  if (s_new) close(s_new);
-  exit(0);
-}
-int main() {
-  struct sockaddr_in sin; // struct sockaddr_in from_sin;
-  signal(SIGINT, &siginthandler); // установка обработчика SIGINT s = socket(AF_INET,
-  SOCK_STREAM, 0);
-  sin.sin_family = AF_INET; // семейство AF_INET sin.sin_addr.s_addr = INADDR_ANY;
-  sin.sin_port = SRV_PORT; // установка порта сервера bind(s, (struct sockaddr *)&sin,
-  sizeof(sin));
-  listen(s, 3); // очередь на сервер – 3 клиента
-  while (1) {
-    from_len = sizeof(from_sin);
-    s_new = accept(s, (struct sockaddr *) &from_sin, &from_len);
-    path_l = read(s_new, path, PATH_SIZE);
-    write(1, path, strlen(path));
-    write(1, " - writing...\n", 14);
-    if (stat(path, &st) == -1) { // если директории нет - создать
-      _mkdir(path);
-    }
-
-    strcat(path, "/tar.tar"); // создаем получаемый архив
-    fd = open(path, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR | S_IXUSR);
-    if (fd == -1) {
-      write(1, "File error\n", 11);
-      return 1;
-    }
-
-    while ((from_len = read(s_new, buf, BUF_SIZE)) != 0) {
-      write(fd, buf, from_len); // записываем данные в файл tar.tar
-    }
-    close(fd); // закрываем файл fd = 0;
-    memcpy(tar, TAR, strlen(TAR));
-    memcpy(tar + strlen(TAR), path, strlen(path));
-    memcpy(tar + strlen(TAR) + strlen(path), " -C ", 4);
-    memcpy(tar_path, path, strlen(path));
-
-    *(strrchr(tar_path, '/') + 1) = '\0';
-
-    memcpy(tar + strlen(TAR) + strlen(path) + 4, tar_path, strlen(tar_path));
-
-    system(tar); // создание команды распаковки и ее выполнение memcpy(rm_tar, RM,
-    strlen(RM)); // удаление архива memcpy(rm_tar+strlen(RM), path, strlen(path));
-    memset(path, '\0', PATH_SIZE);
-    system(rm_tar);
-    shutdown(s_new, 0);
-    write(1, "Ready!\n", 7);
-    s_new = 0;
+/**@brief Получает от клиента последовательность байт, не длиннее 30 и печатает её на экран по
+ * завершении соединения. Клиенту отправляет "Hi, dear!"*/
+int main(int argc, char *argv) {
+  /*создаём сокет*/
+  int s = socket(AF_INET, SOCK_STREAM, 0);
+  if (s < 0) {
+    perror("Error calling socket");
+    return 0;
   }
+
+  /*определяем прослушиваемый порт и адрес*/
+  struct sockaddr_in addr;
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(18666);
+  addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  if (bind(s, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
+    perror("Error calling bind");
+    return 0;
+  }
+
+  /*помечаем сокет, как пассивный - он будет слушать порт*/
+  if (listen(s, 5)) {
+    perror("Error calling listen");
+    return 0;
+  }
+
+  /*начинаем слушать, для соединения создаём другой сокет, в котором можем общаться.*/
+  int s1 = accept(s, NULL, NULL);
+  if (s1 < 0) {
+    perror("Error calling accept");
+    return 0;
+  }
+
+  /*читаем данные из сокета*/
+  char buffer[31];
+  int counter = 0;
+  for (;;) {
+    memset(buffer, 0, sizeof(char) * 31);
+    /*следует помнить, что данные поступают неравномерно*/
+    int rc = recv(s1, buffer, 30, 0);
+    if (rc < 0) {
+      /*чтение может быть прервано системным вызовом, это нормально*/
+      if (errno == EINTR)
+        continue;
+      perror("Can't receive data.");
+      return 0;
+    }
+    if (rc == 0)
+      break;
+    printf("%s\n", buffer);
+  }
+  char response[] = "Hi, dear!";
+  if (sendto(s1, response, sizeof(response), 0, (struct sockaddr *) &addr, sizeof(addr)) < 0)
+    perror("Error sending response");
+  printf("Response send\n");
+  return 0;
 }
