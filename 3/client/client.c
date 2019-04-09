d0f71cef352343d429c8e913e7e4d0a6619d3c5d
Tsaanstu <atasunts.vladimir@yandex.ru>
2019-04-10 01:44:10 +0300

    lab 3 clean

--- 3/client/client.c
+++ 3/client/client.c
@@ -1,25 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <memory.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <string.h>
 #include <stdlib.h>
 #include <arpa/inet.h>
-#include <string.h>
 
 #define SRV_PORT 1234
 #define CLNT_PORT 1235
-#define BUF_SIZE 2048
-#define PATH_SIZE 64
-
 #define RM "rm file_storage.tar"
 #define TAR "tar -c -f ./file_storage.tar "
-#define ERROR -1
 
 int main(int argc, char **argv) {
   system("clear");
@@ -29,20 +17,22 @@ int main(int argc, char **argv) {
   memset(recvBuff, '0', sizeof(recvBuff));
   struct sockaddr_in serv_addr;
 
-  /* Create a socket first */
+  if (argc < 3) {
+    perror("Bad arguments. Example: cprem path.to.src.file host@path.to.dst.dir\n");
+    return 1;
+  }
+
   if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
     printf("\n Error : Could not create socket \n");
     return 1;
   }
 
-  /* Initialize sockaddr_in data structure */
-  char ip[50];
+  char ip[16];
   strcpy(ip, argv[1]);
   serv_addr.sin_family = AF_INET;
-  serv_addr.sin_port = htons(SRV_PORT); // port
+  serv_addr.sin_port = htons(SRV_PORT);
   serv_addr.sin_addr.s_addr = inet_addr(ip);
 
-  /* Attempt a connection */
   if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
     printf("\n Error : Connect Failed \n");
     return 1;
@@ -51,13 +41,10 @@ int main(int argc, char **argv) {
   printf("Connected to ip: %s : %d\n", inet_ntoa(serv_addr.sin_addr), ntohs(serv_addr.sin_port));
 
   size_t TAR_C = strlen(TAR) + strlen(argv[2]);
-  char *tar = (char *) malloc(TAR_C); // команда архивации
+  char *tar = (char *) malloc(TAR_C);
   memcpy(tar, TAR, strlen(TAR));
   memcpy(tar + strlen(tar), argv[2], strlen(argv[2]));
-  system(tar); // команда на архивацию
-
-
-  printf(":: %s\n", tar);
+  system(tar);
 
   FILE *fp = fopen("file_storage.tar", "rb");
   if (fp == NULL) {
@@ -65,7 +52,6 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  system("ls");
   printf("Sending...\n");
   while (1) {
     unsigned char buff[1024] = {0};
@@ -77,17 +63,17 @@ int main(int argc, char **argv) {
 
     if (nread < 1024) {
       if (feof(fp)) {
-        printf("End of file\n");
+        printf("End of file\nSending done\n");
       }
       if (ferror(fp))
         printf("Error reading\n");
       break;
     }
   }
+
   printf("Closing Connection\n");
   close(sockfd);
   shutdown(sockfd, SHUT_WR);
-
   system(RM);
   return 0;
 }
--- 3/server/server.c
+++ 3/server/server.c
@@ -1,90 +1,27 @@
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <unistd.h>
-#include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h>
-#include <errno.h>
-#include <memory.h>
-#include <sys/stat.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
 
-#define SRV_PORT 1234 // порт подключения
-#define BUF_SIZE 4096 // размер буфера
-#define PATH_SIZE 128 // размер пути
-#define RM "rm file_storage.tar" // стандартная утилита удаления (для вызова в системе)
-#define TAR "tar -xvf file_storage.tar" // утилита архиватор
-#define TAR_C 256 // длина команды tar
+#define SRV_PORT 1234
+#define RM "rm file_storage.tar"
+#define TAR "tar -xvf file_storage.tar"
 
 struct stat st = {0};
 int s, s_new = 0;
-int from_len;
 int fd = 0;
-char buf[BUF_SIZE];
-char path[PATH_SIZE];
-char tar[TAR_C];
-char rm_tar[TAR_C];
-char tar_path[TAR_C];
-int path_l, name_l;
-
 struct sockaddr_in c_addr;
+int connfd;
 
-void gotoxy(int x, int y) {
-  printf("%c[%d;%df", 0x1B, y, x);
-}
-
-//void *get_file(int *arg) {
-//  char recvBuff[1024];
-//  int sockfd = (int) *arg;
-//  FILE *fp;
-//  char fname[100];
-//  int bytesReceived = 0;
-//  read(sockfd, fname, 256);
-//  printf("File Name: %s :)\n", fname);
-//  printf("Receiving file...");
-//
-//  fp = fopen(fname, "ab");
-//
-//  if (NULL == fp) {
-//    printf("Error opening file\n");
-//    return 1;
-//  }
-//
-//  long double sz = 1;
-//  while ((bytesReceived = read(sockfd, recvBuff, 1024)) > 0) {
-//    sz++;
-//    gotoxy(0, 4);
-//    printf("Received: %llf Mb", (sz / 1024));
-//    fflush(stdout);
-//    fwrite(recvBuff, 1, bytesReceived, fp);
-//  }
-//
-//  if (bytesReceived < 0) {
-//    printf("\n Read Error \n");
-//  }
-//  printf("\nFile OK....Completed\n");
-//  fclose(fp);
-//}
-
-void siginthandler() { // обработчик Ctrl+C, грамотное отключение сервера
-  write(1, "\nServer is shutting down\n", 25);
-  if (fd) {
-    close(fd);
-  }
-  shutdown(s_new, 0);
-  if (s_new) {
-    close(s_new);
-  }
-  exit(0);
-}
+void upload_text(int x, int y);
+void siginthandler();
 
 int main(int argc, char *argv[]) {
   system("clear");
-  int connfd = 0, err;
+  connfd = 0;
   pthread_t tid;
   struct sockaddr_in serv_addr;
   int listenfd = 0, ret;
@@ -93,11 +30,11 @@ int main(int argc, char *argv[]) {
   size_t clen = 0;
   signal(SIGINT, &siginthandler);
   listenfd = socket(AF_INET, SOCK_STREAM, 0);
+
   if (listenfd < 0) {
     printf("Error in socket creation\n");
     exit(2);
   }
-
   printf("Socket retrieve success\n");
 
   serv_addr.sin_family = AF_INET;
@@ -129,8 +66,6 @@ int main(int argc, char *argv[]) {
     char *fname = (char *) malloc(100);
     strcpy(fname, "file_storage.tar");
     int bytesReceived = 0;
-//    read(sockfd, fname, 256);
-    printf("File Name: %s :)\n", fname);
     printf("Receiving file...");
 
     fp = fopen(fname, "ab");
@@ -143,7 +78,7 @@ int main(int argc, char *argv[]) {
     long double sz = 1;
     while ((bytesReceived = read(sockfd, recvBuff, 1024)) > 0) {
       sz++;
-      gotoxy(0, 4);
+      upload_text(0, 4);
       printf("Received: %llf Mb", (sz / 1024));
       fflush(stdout);
       fwrite(recvBuff, 1, bytesReceived, fp);
@@ -153,13 +88,39 @@ int main(int argc, char *argv[]) {
       printf("\n Read Error \n");
     }
 
-    printf("1) %s\n2) %s\n", tar, rm_tar);
-    system(TAR); // создание команды распаковки и ее выполнение
+    system(TAR);
     system(RM);
 
-    printf("\nFile OK....Completed\n");
+    printf("\nFile OK...\nCompleted\n");
     fclose(fp);
   }
+  return 0;
+}
+
+void upload_text(int x, int y) {
+  printf("%c[%d;%df", 0x1B, y, x);
+}
+
+void siginthandler() {
+  write(1, "\nServer is shutting down\n", 25);
+  close(connfd);
+  if (fd) {
+    close(fd);
+  }
+  shutdown(s_new, 0);
+  if (s_new) {
+    close(s_new);
+  }
+  exit(0);
+}
+
+
+
+
+
+
+
+//  thread?
 //  while (1) {
 //    clen = sizeof(c_addr);
 //    printf("Waiting...\n");
@@ -172,13 +133,37 @@ int main(int argc, char *argv[]) {
 //    if (err != 0)
 //      printf("\ncan't create thread :[%s]", strerror(err));
 //  }
-  close(connfd);
-  return 0;
-}
 
 
-//    system(tar); // создание команды распаковки и ее выполнение
-//    memcpy(rm_tar, RM, strlen(RM)); // удаление архива
-//    memcpy(rm_tar + strlen(RM), path, strlen(path));
-//    memset(path, '\0', PATH_SIZE);
-//    system(rm_tar);
+//void *get_file(int *arg) {
+//  char recvBuff[1024];
+//  int sockfd = (int) *arg;
+//  FILE *fp;
+//  char fname[100];
+//  int bytesReceived = 0;
+//  read(sockfd, fname, 256);
+//  printf("File Name: %s :)\n", fname);
+//  printf("Receiving file...");
+//
+//  fp = fopen(fname, "ab");
+//
+//  if (NULL == fp) {
+//    printf("Error opening file\n");
+//    return 1;
+//  }
+//
+//  long double sz = 1;
+//  while ((bytesReceived = read(sockfd, recvBuff, 1024)) > 0) {
+//    sz++;
+//    upload_text(0, 4);
+//    printf("Received: %llf Mb", (sz / 1024));
+//    fflush(stdout);
+//    fwrite(recvBuff, 1, bytesReceived, fp);
+//  }
+//
+//  if (bytesReceived < 0) {
+//    printf("\n Read Error \n");
+//  }
+//  printf("\nFile OK....Completed\n");
+//  fclose(fp);
+//}
